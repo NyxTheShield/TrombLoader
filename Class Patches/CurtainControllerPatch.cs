c6535538f96f6d4c11fa840ba3b3dd01e7a43444
NyxTheShield <felipe.montero.13@sansano.usm.cl>
2022-09-25 20:04:55 -0300

    Fix path issue and Exitting tracks breaking the game

--- Class Patches/CurtainControllerPatch.cs
+++ Class Patches/CurtainControllerPatch.cs
@@ -12,32 +12,29 @@ public class CurtainControllerPatch
 
     static bool Prefix(CurtainController __instance)
     {
-        if (__instance.gamec.levelnum == GlobalVariables.data_trackrefs.Length - 1 && !GlobalVariables.localsave.progression_trombone_champ)
+        LeanTween.cancelAll();
+        Cursor.lockState = CursorLockMode.None;
+        if (__instance.gamec.quitting && !__instance.gamec.retrying && GlobalVariables.scene_destination != "freeplay")
         {
-            LeanTween.cancelAll();
-            Cursor.lockState = CursorLockMode.None;
-            if (__instance.gamec.quitting && !__instance.gamec.retrying && GlobalVariables.scene_destination != "freeplay")
-            {
-                SceneManager.LoadScene("levelselect");
-                return false;
-            }
-            if (__instance.gamec.quitting && __instance.gamec.retrying)
-            {
-                SceneManager.LoadScene("gameplay");
-                return false;
-            }
-            if (GlobalVariables.scene_destination == "freeplay")
-            {
-                SceneManager.LoadScene("home");
-                return false;
-            }
-            if (GlobalVariables.data_trackrefs[__instance.gamec.levelnum] == "einefinal" && !GlobalVariables.localsave.progression_trombone_champ)
-            {
-                SceneManager.LoadScene("finallevel_fail");
-                return false;
-            }
-            SceneManager.LoadScene("points");
+            SceneManager.LoadScene("levelselect");
+            return false;
         }
+        if (__instance.gamec.quitting && __instance.gamec.retrying)
+        {
+            SceneManager.LoadScene("gameplay");
+            return false;
+        }
+        if (GlobalVariables.scene_destination == "freeplay")
+        {
+            SceneManager.LoadScene("home");
+            return false;
+        }
+        if (GlobalVariables.data_trackrefs[__instance.gamec.levelnum] == "einefinal" && !GlobalVariables.localsave.progression_trombone_champ)
+        {
+            SceneManager.LoadScene("finallevel_fail");
+            return false;
+        }
+        SceneManager.LoadScene("points");
         return false;
     }
 }
