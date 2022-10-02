ad6c68f8912c8ef83704ac2781cd39bd47ec99c3
NyxTheShield <felipe.montero.13@sansano.usm.cl>
2022-10-02 02:43:08 -0300

    Added LeanTweenHelper class for future bg modding

---
+++ Data/LeanTweenHelper.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using UnityEngine;
+
+namespace TrombLoader.Data;
+
+public class LeanTweenHelper:MonoBehaviour
+{
+    public LeanTweenHelperType tweenType;
+    public Vector3 targetValue;
+    public float time;
+    public LeanTweenType easeType = LeanTweenType.linear;
+    public LeanTweenType loopType = LeanTweenType.clamp;
+    public int loopCount = 0;
+    public bool runOnStart = false;
+    public LeanTweenHelper invokeOnComplete;
+    
+    
+    private Action tweenAction;
+    
+    public void Start()
+    {
+        switch (tweenType)
+        {
+            case LeanTweenHelperType.MOVELOCAL:
+                tweenAction = () => LeanTween.moveLocal(this.gameObject, targetValue, time).setEase(easeType).setLoopType(loopType).setOnComplete(() => invokeOnComplete.DoTween());
+                break;
+            case LeanTweenHelperType.MOVEGLOBAL:
+                tweenAction = () => LeanTween.move(this.gameObject, targetValue, time).setEase(easeType).setLoopType(loopType).setOnComplete(() => invokeOnComplete.DoTween());
+                break;
+            case LeanTweenHelperType.ROTATELOCAL:
+                tweenAction = () => LeanTween.rotateLocal(this.gameObject, targetValue, time).setEase(easeType).setLoopType(loopType).setOnComplete(() => invokeOnComplete.DoTween());
+                break;
+            case LeanTweenHelperType.ROTATEGLOBAL:
+                tweenAction = () => LeanTween.rotate(this.gameObject, targetValue, time).setEase(easeType).setLoopType(loopType).setOnComplete(() => invokeOnComplete.DoTween());
+                break;
+            case LeanTweenHelperType.SCALE:
+                tweenAction = () => LeanTween.scale(this.gameObject, targetValue, time).setEase(easeType).setLoopType(loopType).setOnComplete(() => invokeOnComplete.DoTween());
+                break;
+        }
+        if (runOnStart) DoTween();
+    }
+
+    public void DoTween()
+    {
+        tweenAction.Invoke();
+    }
+
+    public enum LeanTweenHelperType
+    {
+        MOVELOCAL=0,
+        MOVEGLOBAL=1,
+        ROTATELOCAL=2,
+        ROTATEGLOBAL=3,
+        SCALE =4
+    }
+}
+
