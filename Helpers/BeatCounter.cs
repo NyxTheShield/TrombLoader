5438c143883a5f0707a41f576875dd4ab61b1eb0
NyxTheShield <felipe.montero.13@sansano.usm.cl>
2022-10-17 18:09:42 -0300

    Granular Beat Counter Extension

--- Helpers/BeatCounter.cs
+++ Helpers/BeatCounter.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -6,29 +8,55 @@ namespace TrombLoader.Helpers;
 
 public class BeatCounter:MonoBehaviour
 {
-    public List<int> targetBeats;
-    public int currentBeat;
+    public List<float> targetBeats;
+    public float currentBeat;
     public UnityEvent actionToExecute;
-
+    private bool isInitialized = false;
+    public float bpm;
     public void IncrementBeat()
     {
-        currentBeat += 1;
-        foreach (int targetBeat in targetBeats)
+        if (!isInitialized)
         {
-            if (currentBeat == targetBeat)
+            isInitialized = true;
+            return;
+        }
+
+        float EighthNoteInterval = 0.125f;
+        
+        //Checks for Beat
+        Debug.Log($"Checking for Beat Number:"+ currentBeat);
+        CheckNote(currentBeat);
+        //Checks for 1/8, 2/8 ....... 7/8 of a beat
+        for (int i = 1; i < 8; i++)
+        {
+            var newTime = EighthNoteInterval * i;
+            var timeInSeconds = newTime * 60 / bpm;
+            LeanTween.value(0, 1, timeInSeconds).setOnComplete(() =>
             {
-                actionToExecute?.Invoke();
-            }
+                CheckNote(currentBeat+ newTime);
+                Debug.Log($"Checking for Beat Number:"+ (currentBeat+newTime));
+            });
+        }
+        currentBeat += 1;
+    }
+    
+    void CheckNote(float target)
+    {
+        if (target >= targetBeats[0])
+        {
+            actionToExecute?.Invoke();
+            targetBeats.RemoveAt(0);
         }
     }
+
 
     [ContextMenu("Parse String")]
     public void ParseFromString(){
         string toParse = GUIUtility.systemCopyBuffer;
-        targetBeats = new List<int>();
+        targetBeats = new List<float>();
         foreach (string number in toParse.Replace("[", "").Replace("]", "").Split(','))
         {
-            targetBeats.Add(int.Parse(number));
+            targetBeats.Add(float.Parse(number, CultureInfo.InvariantCulture));
         }
     }   
 }
---
+++ Helpers/SceneSortingHelper.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace TrombLoader.Helpers;
+
+public class SceneSortingHelper:MonoBehaviour
+{
+    public TransparencySortMode sortingMode = TransparencySortMode.Perspective;
+    public Vector3 sortingAxis = new Vector3(0.0f, 1.0f, 1.0f);
+    
+    public void Start()
+    {
+        GraphicsSettings.transparencySortMode = sortingMode;
+        GraphicsSettings.transparencySortAxis = sortingAxis;
+    }
+}
