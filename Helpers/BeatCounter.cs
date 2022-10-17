7b8f4ff9be89b6d0fe3537855a7e7d4f20189bc3
NyxTheShield <felipe.montero.13@sansano.usm.cl>
2022-10-17 13:55:16 -0300

    Added Beat Counter event invoker

---
+++ Helpers/BeatCounter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace TrombLoader.Helpers;
+
+public class BeatCounter:MonoBehaviour
+{
+    public List<int> targetBeats;
+    public int currentBeat;
+    public UnityEvent actionToExecute;
+
+    public void IncrementBeat()
+    {
+        currentBeat += 1;
+        foreach (int targetBeat in targetBeats)
+        {
+            if (currentBeat == targetBeat)
+            {
+                actionToExecute?.Invoke();
+            }
+        }
+    }
+
+    [ContextMenu("Parse String")]
+    public void ParseFromString(){
+        string toParse = GUIUtility.systemCopyBuffer;
+        targetBeats = new List<int>();
+        foreach (string number in toParse.Replace("[", "").Replace("]", "").Split(','))
+        {
+            targetBeats.Add(int.Parse(number));
+        }
+    }   
+}
